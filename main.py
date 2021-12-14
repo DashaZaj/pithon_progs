c53947c385a720fd508c082dac25796b1f26082d
Dasha_Zajc <no@no.no>
2021-12-14 11:13:50 +0300

    invalid program fixed

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 n = int(input())
-k = int(input())
-mask = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a = [0]*n
+mask = a
+comb = []
+lis = []
 
-j=-1
-for i in range(k):
-    if j < mask[i] < n- k + i:
-        j = i
-mask[j] += 1
-for l in range(j+1, k):
-    mask[l] = j+1+l
+for k in range(1, n+1):
+    for i in range(2**n):
+        if mask.count(1) == k:
+            for m in range(n):
+                if mask[m] == 1:
+                    comb.append(b[m])
+            comb.sort()
+            lis.append(comb)
+        comb = []
+        cur_sum = a[-1] + 1
+        up = cur_sum // 2
+        mask = [cur_sum % 2]
+        for j in range(n-2, -1, -1):
+            cur_sum = a[j] + up
+            ans_1 = cur_sum % 2
+            mask = [ans_1] + mask
+            up = cur_sum // 2
+        a = mask
 
-for x in mask:
-    print(x, end=' ')
-print()
+lis.sort()
+for x in lis:
+    for y in x:
+        print(y, end=" ")
+    print()
