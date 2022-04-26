035089ef1e85f1340285834e17402c3b071bbd67
Zajceva Darya <no@no.no>
2022-04-26 12:11:51 +0300

    changed

--- SQLite.py
+++ SQLite.py
@@ -1,17 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-#conn.execute('''create table test_table
-#            (
-#            id integer primary key not NULL,
-#            data text not NULL
-#            );''')
-#conn.commit()
-#conn.execute("insert into test_table values(1, 'first'), (2, 'second');")
-#conn.commit()
-cursor = conn.cursor()
-cursor.execute('''select * from test_table;''')
-r = cursor.fetchall()
-for x in r:
-    print(x)
-conn.close()
+def db_init():
+    from os.path import exists
+    if exists('rgd.db'):  # TODO: are all tables okay
+        return
+    conn = sqlite3.connect('rgd.db')
+    conn.execute('''create table locomotive_types
+            (
+            locomotive_type_id integer primary key not NULL,
+            type_name text not NULL
+            );''')
+    # TODO: all the tables
+    conn.commit()
+    conn.close()
+
